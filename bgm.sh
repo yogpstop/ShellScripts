efa2ddb0be2a87bf09bad273ac9824edf102ee5e
yogpstop <git@yogpc.com>
2014-01-13 16:55:45 +0900

    Fix bgm player

--- bgm.sh
+++ bgm.sh
@@ -19,12 +19,16 @@ for arg in "$@" ; do
     done <"${arg}"
   fi
 done
-trap '' SIGINT
 fl=(`echo "${fl[*]}" | shuf`)
 for line in "${fl[@]}" ; do
   str=${line##*/}
   echo "Now playing... ${str%.*}"
-  flac -cds "${line}" | chrt -r 98 dd ibs=2G iflag=fullblock 2>/dev/null | chrt -r 99 aplay -D rt -q --period-size=48 --buffer-size=144 -
+  flac -cds "${line}" | chrt -r 98 dd ibs=2G iflag=fullblock 2>/dev/null | \
+    chrt -r 99 aplay -D rt -q --period-size=48 --buffer-size=144 - && \
+    kill `ps | grep "cat" | sed -e "s/ \([0-9]*\).*/\1/"` &>/dev/null &
+  cat
+  kill `ps | grep -E "aplay|flac|dd|chrt" | \
+    sed -e "s/ \([0-9]*\).*/\1/"` &>/dev/null
+  wait `ps | grep "aplay" | sed -e "s/ \([0-9]*\).*/\1/"`
 done
 exit 0
-
