e4eabda99c9091c3c2e4d91b35c9f18381814cdd
yogpstop <git@yogpc.com>
2014-03-07 01:30:05 +0900

    Update bgm player

--- bgm.sh
+++ bgm.sh
@@ -7,6 +7,8 @@ for arg in "$@" ; do
   arg=`readlink -mq "${arg}"`
   if [[ "${arg}" =~ .*\.[Ff][Ll][Aa][Cc] ]] ; then
     fl+=(${arg})
+  elif [[ "${arg}" =~ .*\.[Ww][Aa][Vv] ]] ; then
+    fl+=(${arg})
   elif [ -d "${arg}" ] ; then
     fl+=(`find "${arg}" -type f -iname "*.flac"`)
   elif [ -f "${arg}" ] ; then
@@ -23,9 +25,15 @@ fl=(`echo "${fl[*]}" | shuf`)
 for line in "${fl[@]}" ; do
   str=${line##*/}
   echo "Now playing... ${str%.*}"
-  flac -cds "${line}" | chrt -r 98 dd ibs=2G iflag=fullblock 2>/dev/null | \
-    chrt -r 99 aplay -D rt -q --period-size=48 --buffer-size=144 - && \
-    kill -SIGINT `ps | grep "cat" | sed -e "s/ \([0-9]*\).*/\1/"` &>/dev/null &
+  if [[ "${line}" =~ .*\.[Ff][Ll][Aa][Cc] ]] ; then
+    flac -cds "${line}" | chrt -r 98 dd ibs=2G iflag=fullblock 2>/dev/null | \
+      chrt -r 99 aplay -D rt -q --period-size=48 --buffer-size=144 - && \
+      kill -SIGINT `ps | grep "cat" | sed -e "s/ \([0-9]*\).*/\1/"` &>/dev/null &
+  elif [[ "${line}" =~ .*\.[Ww][Aa][Vv] ]] ; then
+    chrt -r 98 dd "if=${line}" ibs=2G iflag=fullblock 2>/dev/null | \
+      chrt -r 99 aplay -D rt -q --period-size=48 --buffer-size=144 - && \
+      kill -SIGINT `ps | grep "cat" | sed -e "s/ \([0-9]*\).*/\1/"` &>/dev/null &
+  fi
   cat
   kill -SIGINT `ps | grep -E "aplay|flac|dd|chrt" | \
     sed -e "s/ \([0-9]*\).*/\1/"` &>/dev/null
